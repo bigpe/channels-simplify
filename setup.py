b4baec2ccfb88bc03f0a83d1eb9c2a80dc208353
Aleksandr Sokolov <52139992+bigpe@users.noreply.github.com>
2022-06-23 00:29:23 +1000

    Feature: Requirements for package

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+from pip._internal.req import parse_requirements
 import setuptools
 
-setuptools.setup()
+install_requirements = parse_requirements('requirements.txt', session='install')
+
+requirements = [ir.requirement for ir in install_requirements]
+
+setuptools.setup(install_requires=requirements)
